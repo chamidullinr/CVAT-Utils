3a21866ef32196b2898bdbef6d633958636b0688
Lukas Picek <lukaspicek@gmail.com>
2023-10-11 09:04:29 +0200

    chore: flake, isort, black

--- cvat_utils/utils/masking.py
+++ cvat_utils/utils/masking.py
@@ -2,13 +2,14 @@ import cv2
 import numpy as np
 from PIL import Image, ImageDraw
 
+
 def get_foreground_background(image, points):
     height, width, _ = image.shape
-    img = Image.new('L', (int(width), int(height)), 0)
+    img = Image.new("L", (int(width), int(height)), 0)
     ImageDraw.Draw(img).polygon(points, outline=1, fill=1)
     mask = np.array(img)
 
-    foreground_mask = np.logical_not(mask).astype('uint8')
+    foreground_mask = np.logical_not(mask).astype("uint8")
 
     result_foreground = cv2.bitwise_and(image, image, mask=mask)
     result_backround = cv2.bitwise_and(image, image, mask=foreground_mask)
